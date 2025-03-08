39040456b3591322ce66b68804238ec704323ac8
avpc22 <avportilloc@ufpso.edu.co>
2025-03-08 13:00:20 -0500

    Ejercicio Quiz

---
+++ src/Quiz.java
@@ -0,0 +1,11 @@
+public class Quiz {
+    public static void main(String[] args) {
+        //representacion textual de la hora en formato HH:MM
+
+    System.out.println("ingrese la hora: ");
+    int 
+    System.out.println("ingrese los minutos: ");
+    int 
+    
+    }
+}
